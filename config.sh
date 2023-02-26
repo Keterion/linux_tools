30ef44bc3103db4d0cf410f956ced8ecf85da4fd
Keterion <HighTech-Nik@web.de>
2023-02-26 19:55:22 +0100

    split to multiple files

---
+++ config.sh
@@ -0,0 +1,18 @@
+mkdir ~/.config/zellij
+zellij setup --dump-config > ~/.config/zellij/config.kdl
+
+mkdir -p ~/.config && touch ~/.config/starship.toml
+
+CONSOLE_CONFIG = ~/.bashrc
+
+echo 'alias ls="lsd"' >> $CONSOLE_CONFIG
+echo 'alias cat="bat"' >> $CONSOLE_CONFIG
+echo 'alias grep="rg"' >> $CONSOLE_CONFIG
+echo "fsi" >> $CONSOLE_CONFIG
+
+echo "created aliases for ls -> lsd; cat -> bat; grep -> rg"
+
+echo 'eval "$(starship init bash)"' >> $CONSOLE_CONFIG
+echo "set spaceship as terminal prompt"
+
+ ~/.bashrc
--- install.sh
+++ install.sh
@@ -1,34 +1,10 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source "$HOME/.cargo/env"
-CONSOLE_CONFIG=~/.bashrc`
-
-# install sccache for quicker installs
-cargo install sccache
-printf "[build]\nrustc-wrapper = '%s/.cargo/bin/sccache'" $HOME  >> $HOME/.cargo/config.toml # add sccache to the cargo build so it is used
 # downloads cmake for following packages
 yay -S cmake
 
 # installs some useful packages
 cargo install coreutils lsd du-dust bat ripgrep porsmo speedtest-rs wiki-tui zellij gitoxide helix spaceship fsi
 echo "Installed: coreutils, lsd, du-dust, bat, ripgrep, irust, porsmo, speedtest-rs, wiki-tui, zellij, gitoxide, helix, spaceship"
-# Creating configs
-mkdir ~/.config/zellij
-mkdir -p ~/.config && touch ~/.config/starship.toml
-zellij setup --dump-config > ~/.config/zellij/config.kdl
-
-# create aliases
-echo 'alias ls="lsd"' >> $CONSOLE_CONFIG
-echo 'alias cat="bat"' >> $CONSOLE_CONFIG
-echo 'alias grep="rg"' >> $CONSOLE_CONFIG
-echo "fsi" >> $CONSOLE_CONFIG
-# notify of alias creation
-echo "created aliases for ls -> exa; cat -> bat; grep -> ripgrep;"
 
 #reload config
-source ~/.bashrc
-
-# customization
 cargo install spaceship
-echo 'eval "$(starship init bash)"' >> $CONSOLE_CONFIG
-echo "set spaceship as terminal prompt"
-yay -S neofetch
+echo "installed spaceship"
