b3904fd6c1e9f027906bd1a23869d44d832c4047
Keterion <HighTech-Nik@web.de>
2023-02-27 17:07:16 +0100

    Change to only copy config files not modify them

--- endeavouros/config.sh
+++ endeavouros/config.sh
@@ -1,21 +1,7 @@
-mkdir ~/.config/zellij
-zellij setup --dump-config > ~/.config/zellij/config.kdl
-
-mkdir -p ~/.config && touch ~/.config/starship.toml
-
 sudo ln -s ~/.local/kitty.app/bin/kitty /bin/
 sudo ln -s ~/.local/kitty.app/bin/kitten /bin/
 
-CONSOLE_CONFIG = ~/.zshrc
-CONSOLE_NAME = zsh
-
-echo 'alias ls="lsd"' >> $CONSOLE_CONFIG
-echo 'alias cat="bat"' >> $CONSOLE_CONFIG
-echo 'alias grep="rg"' >> $CONSOLE_CONFIG
-
-echo "created aliases for ls -> lsd; cat -> bat; grep -> rg"
-
-printf 'eval "$(zoxide init %s)"' $CONSOLE_NAME >> $CONSOLE_CONFIG
+cp -r ./configs/kitty ~/.config/ 
 
 source $CONSOLE_CONFIG
 
--- endeavouros/install.sh
+++ endeavouros/install.sh
@@ -2,8 +2,8 @@
 yay -S cmake
 
 # installs some useful packages
-cargo install coreutils lsd du-dust bat ripgrep porsmo speedtest-rs wiki-tui zellij gitoxide helix spaceship fsi
-echo "Installed: coreutils, lsd, du-dust, bat, ripgrep, irust, porsmo, speedtest-rs, wiki-tui, zellij, gitoxide, helix, spaceship"
+cargo install coreutils lsd du-dust bat ripgrep porsmo speedtest-rs wiki-tui gitoxide helix spaceship fsi
+echo "Installed: coreutils, lsd, du-dust, bat, ripgrep, irust, porsmo, speedtest-rs, wiki-tui, gitoxide, helix, spaceship"
 
 #reload config
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
