521342b67764fccef7cc7d8b8f91526a21ff37af
Keterion <100532848+Keterion@users.noreply.github.com>
2023-02-27 17:49:56 +0100

    Update install.sh to work

--- endeavouros/install.sh
+++ endeavouros/install.sh
@@ -2,8 +2,9 @@
 yay -S cmake
 
 # installs some useful packages
-cargo install coreutils lsd du-dust bat ripgrep porsmo speedtest-rs wiki-tui gitoxide helix spaceship fsi
-echo "Installed: coreutils, lsd, du-dust, bat, ripgrep, irust, porsmo, speedtest-rs, wiki-tui, gitoxide, helix, spaceship"
+cargo install coreutils lsd du-dust bat ripgrep porsmo speedtest-rs wiki-tui gitoxide fsi
+yay -S helix
+echo "Installed: coreutils, lsd, du-dust, bat, ripgrep, irust, porsmo, speedtest-rs, wiki-tui, gitoxide"
 
 #reload config
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
